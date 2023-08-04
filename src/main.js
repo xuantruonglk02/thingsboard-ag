457529259fd19ef5b8fa3d6034571f2d33813787
Truong <xuantruonglk02@gmail.com>
2023-08-05 00:51:11 +0700

    update stream

--- src/main.js
+++ src/main.js
@@ -17,6 +17,24 @@ function streamData() {
         MapHumiditySensorWithFirebase,
         process.env[ConfigKeys.THINGSBOARD_FIELD1_HUMIDITY_SENSOR_ACCESS_TOKEN]
     );
+    // Field2 - Humidity Sensor
+    stream(
+        process.env[ConfigKeys.FIREBASE_FIELD2_HUMIDITY_SENSOR_DATA_REF],
+        MapHumiditySensorWithFirebase,
+        process.env[ConfigKeys.THINGSBOARD_FIELD2_HUMIDITY_SENSOR_ACCESS_TOKEN]
+    );
+    // Field3 - Humidity Sensor
+    stream(
+        process.env[ConfigKeys.FIREBASE_FIELD3_HUMIDITY_SENSOR_DATA_REF],
+        MapHumiditySensorWithFirebase,
+        process.env[ConfigKeys.THINGSBOARD_FIELD3_HUMIDITY_SENSOR_ACCESS_TOKEN]
+    );
+    // Field4 - Humidity Sensor
+    stream(
+        process.env[ConfigKeys.FIREBASE_FIELD4_HUMIDITY_SENSOR_DATA_REF],
+        MapHumiditySensorWithFirebase,
+        process.env[ConfigKeys.THINGSBOARD_FIELD4_HUMIDITY_SENSOR_ACCESS_TOKEN]
+    );
     // Weather Station
     stream(
         process.env[ConfigKeys.FIREBASE_WEATHER_STATION_DATA_REF],
